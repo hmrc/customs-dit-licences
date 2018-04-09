efa457068314b823cab65638e0d5438788e62c73
Paul Blake <paul.blake@digital.hmrc.gov.uk>
2018-04-09 09:33:39 +0100

    CDD-1459: removed unnecessary test causing build to fail

--- test/integration/IntegrationTestSpec.scala
+++ test/integration/IntegrationTestSpec.scala
@@ -19,14 +19,9 @@ package integration
 import org.scalatest.concurrent.Eventually
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
 import uk.gov.hmrc.play.test.UnitSpec
-import util.RequestHeaders.AuthHeaderTokenInternal
 
 trait IntegrationTestSpec extends UnitSpec
   with BeforeAndAfterEach with BeforeAndAfterAll with Eventually with GuiceOneAppPerSuite {
 
-  override implicit lazy val app: Application =
-    new GuiceApplicationBuilder().configure(Map("auth.token.internal" -> AuthHeaderTokenInternal)).build()
 }
